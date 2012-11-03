b22a8056cd1a330b2eae8d7c6cdd320ca90366f1
Allan Denot <adenot@gmail.com>
2012-11-03 13:22:19 +0000

    Added stderr

--- exec.sh
+++ exec.sh
@@ -4,14 +4,14 @@
 if [ "$1" == "ex" ]
 then
 
-	echo executing $2 $3 $4 > /tmp/exec.tmp
-	$3 $4 $5 $6 $7 > $2
+	echo executing $3 $4 $5 $6 $7 $8 $9 > /tmp/exec.tmp
+	$3 $4 $5 $6 $7 $8 $9 > $2 2>&1
 	#echo > /tmp/exec.end
 
 else 
 	#rm -fr /tmp/exec.out
 	#rm -fr /tmp/exec.end
-	screen -d -m $0 ex $1 $2 $3 $4 $5 $6 $7 
+	screen -d -m $0 ex $1 $2 $3 $4 $5 $6 $7 $8 $9
 
 	#while [ ! -f /tmp/exec.end ]
 	#do
