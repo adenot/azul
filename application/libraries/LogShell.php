ccb2c971b11b8d06e8f533f2cc2571ff60d5047b
Allan Denot <adenot@gmail.com>
2012-11-03 13:25:19 +0000

    Adding LogShell class

---
+++ application/libraries/LogShell.php
@@ -0,0 +1,27 @@
+<?php
+
+class LogShell {
+
+	public static function create($op) 
+	{
+		$id = uniqid($op);
+	
+		$file = Config::get("sites.log_dir"). "/" . $id . ".log";
+
+		return array($id, $file);
+	}
+
+	public static function read($id, $offset) 
+	{
+
+		$file = Config::get("sites.log_dir"). "/" . $id . ".log";
+
+		$content = file_get_contents($file, false, NULL, $offset);
+		
+		$offset = $offset + strlen($content);
+		
+		return array($content, $offset);
+		
+    }
+
+}
