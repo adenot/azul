95f5d840447f12d476ee7f770625c03b5c0e6e1d
adenot <adenot@gmail.com>
2012-11-09 04:46:20 +0000

    Adding migration file for users table

---
+++ application/migrations/2012_11_09_001153_create_users.php
@@ -0,0 +1,30 @@
+<?php
+
+class Create_Users {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('email', 100);
+			$table->string('password', 100);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
